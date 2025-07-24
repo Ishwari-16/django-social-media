be44e51f479ea44467d0375209f4ee8c81e2f710
unknown <ishwaribarole@gmail.com>
2025-07-24 20:23:41 +0530

    Updated views, URLs, and templates

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,35 @@
-console.log("Hello world <3")
+$.ajaxSetup({
+      beforeSend: function beforeSend(xhr, settings) {
+          function getCookie(name) {
+             let cookieValue = null;
+
+
+            if (document.cookie && document.cookie !== '') {
+                  const cookies = document.cookie.split(';');
+
+                   for (let i = 0; i < cookies.length; i++) {
+                      const cookie = cookies[i].trim();
+            
+                      // If the cookie string starts with the name we want
+                      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                     }
+                 }
+           }
+
+           return cookieValue;
+        }
+
+        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+    },
+});      
+
+
+$(document).on("click","js-toggle-modal", function(e){
+      e.preventDefault()
+      console.log("Hello I was clicked")
+      //$(".js-model").toggleClass("hidden")
+})
