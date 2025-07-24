5fc2858965ab1985dea751e3046633de4dbcc875
unknown <ishwaribarole@gmail.com>
2025-07-24 22:23:49 +0530

    Updated views, URLs, and templates

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -28,8 +28,36 @@ $.ajaxSetup({
 });      
 
 
-$(document).on("click","js-toggle-modal", function(e){
-      e.preventDefault()
-      console.log("Hello I was clicked")
-      //$(".js-model").toggleClass("hidden")
+$(document).on("click", ".js-toggle-modal", function(e){
+      e.preventDefault();
+      $(".js-model").toggleClass("hidden");
 })
+.on("click", ".js-submit", function(e){
+    e.preventDefault()
+    console.log("Submit me?")
+ })
+const $btn = $(this)
+const text=$(".js-post-text").val().trim()
+if(!text.length){
+    return false 
+}
+
+$btn.prop("disabled", true).text("Posting!")
+$.ajax({
+    type:'POST',
+    url:$(".js-post-text").data("post-url"),
+    data:{
+        text:text
+    },
+success:(dataHtml)=>{
+    $(".js-modal").addClass("hidden");
+    $(" #posts-container").prepend(dataHtml);
+    $btn.prop("disabled",false).text("New Post");
+    $(".js-post-text").val('')
+},
+error:(error)=>{
+    console.warn(error)
+$btn.prop("disabled", false).text("Error");
+}
+});
+
