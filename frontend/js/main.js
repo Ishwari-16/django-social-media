e6dcf9b6062a4b3b427cfdadd44acd02b44a065b
unknown <ishwaribarole@gmail.com>
2026-06-23 01:21:02 +0530

    fix follow route

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -27,36 +27,40 @@ $.ajaxSetup({
     },
 });      
 
-$(document).on("click", ".js-follow", function(e){
-    e.preventDefault();
+$(document).on("click", ".js-toggle-modal", function(e){
+      e.preventDefault();
+      $("#post-modal").toggleClass("hidden");
+      $("#post-modal").toggleClass("flex");
+})
+    .on("click", ".js-submit", function(e){
+        e.preventDefault()
+        console.log("Submit me?")
+        const text= $(".js-post-text").val().trim()
+        const $btn = $(this)
 
-    const $btn = $(this);
+        if(!text.length){
+            return false; 
+        }
 
-    $.ajax({
-        type: "POST",
-        url: $btn.data("url"),
-        data: {
-            action: $btn.attr("data-action"),
-            username: $btn.attr("data-username")
+        $btn.prop("disabled", true).text("Posting!")
+        $.ajax({
+            type:'POST',
+            url:$(".js-post-text").data("post-url"),
+            data:{
+                text:text
+            },
+        success:(dataHtml) => {
+            $("#post-modal").addClass("hidden");
+            $("#posts-container").prepend(dataHtml);
+            $btn.prop("disabled",false).text("Post");
+            $(".js-post-text").val('')
         },
-
-        success: function(data){
-
-            $(".js-follow-text").text(data.wording);
-
-            if($btn.attr("data-action") === "follow"){
-                $btn.attr("data-action","unfollow");
-            } else {
-                $btn.attr("data-action","follow");
-            }
-        },
-
-        error: function(xhr){
-            console.log(xhr.responseText);
+        error:(error) => {
+            console.warn(error)
+            $btn.prop("disabled", false).text("Error");
         }
     });
-});
-    
+})
 success:(data) => {
     $btn.find(".js-follow-text").text(data.wording);
 
