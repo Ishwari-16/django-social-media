f8bbd066120428d1912e2db8dcbde99db47fc1f7
unknown <ishwaribarole@gmail.com>
2026-06-23 01:12:54 +0530

    Fix follow button

--- til/settings.py
+++ til/settings.py
@@ -54,17 +54,18 @@ INSTALLED_APPS = [
     'sorl.thumbnail',
     'messaging.apps.MessagingConfig',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-     'whitenoise.middleware.WhiteNoiseMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 ]
 
