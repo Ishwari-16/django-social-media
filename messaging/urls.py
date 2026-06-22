9d538116846ba872673037ce0b356f902e5cb055
unknown <ishwaribarole@gmail.com>
2026-06-23 01:18:49 +0530

    fix follow route

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from .views import SendMessageView
 
 app_name = "messaging"
 
--- profiles/urls.py
+++ profiles/urls.py
@@ -17,14 +17,11 @@ app_name = 'profiles'
 urlpatterns = [
     path('', homepage, name='home'),
     path('<str:username>/', ProfileDetailView.as_view(), name='detail'),
+    path('<str:username>/follow/', FollowView.as_view(), name='follow'),
     path('edit/', EditProfileView.as_view(), name='edit'),
     path('edit/image/', EditProfileImageView.as_view(), name='edit_image'),
     path('edit/cover/', EditCoverPhotoView.as_view(), name='edit_cover'),
     path('edit-name-username/', edit_name_username, name='edit_name_username'),
     path('search/', search, name='search'),
-    path(
-    "<slug:username>/follow/",
-    FollowView.as_view(),
-    name="follow",
-),
+    
 ]
