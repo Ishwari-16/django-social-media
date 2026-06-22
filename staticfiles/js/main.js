54949e50d06346a2f7f58f377633726b79b4ecd0
unknown <ishwaribarole@gmail.com>
2026-06-23 02:13:06 +0530

    fix static js

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -1,1 +1,24 @@
-console.log("Hello world <3")
+console.log("MAIN JS LOADED");
+
+$(document).on("click", ".js-follow", function(e){
+    e.preventDefault();
+
+    alert("FOLLOW CLICKED");
+
+    const btn = $(this);
+
+    $.ajax({
+        type: "POST",
+        url: btn.data("url"),
+        data: {
+            username: btn.data("username"),
+            action: btn.data("action")
+        },
+        success: function(data){
+            alert("SUCCESS");
+        },
+        error: function(xhr){
+            alert("ERROR " + xhr.status);
+        }
+    });
+});
