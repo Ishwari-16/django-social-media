e86da546f03c595a45ffb4b5f9e036c220e1ab6b
unknown <ishwaribarole@gmail.com>
2026-06-23 02:33:27 +0530

    fix static path

--- til/settings.py
+++ til/settings.py
@@ -146,7 +146,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "frontend",
 ]
 # Media files
 MEDIA_URL = '/media/'
